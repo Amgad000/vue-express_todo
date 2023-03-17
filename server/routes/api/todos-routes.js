5659ee1f6822d068dcfbf367eda7b0839be24210
Amgad000 <almuhamady.amgad001@gmail.com>
2023-03-17 20:09:46 +0200

    added update methods to db

--- server/routes/api/todos-routes.js
+++ server/routes/api/todos-routes.js
@@ -1,4 +1,3 @@
-const {response} = require("express");
 const express = require("express");
 const router = express.Router();
 
@@ -34,6 +33,26 @@ router.delete("/:id", (req, res) => {
     .then((response) => {
       // console.log(response);
       res.status(200).send();
+    })
+    .catch((err) => console.log(err));
+});
+
+// Get specific
+router.get("/:id", (req, res) => {
+  Todo.findById(req.params.id)
+    .then((response) => res.send(response))
+    .catch((err) => res.send(err));
+});
+// UPDATE or upsert
+// upsert (if not find id it will create new todo)
+router.put("/edit/:id", (req, res) => {
+  Todo.findByIdAndUpdate(req.params.id, req.body, {
+    setDefaultsOnInsert: true,
+    new: true,
+    upsert: true,
+  })
+    .then((response) => {
+      res.status(200).send(response);
     })
     .catch((err) => console.log(err));
 });
